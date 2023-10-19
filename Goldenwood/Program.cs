5e29044c8ba1d982a3ad4cedc5c0f081137d876d
Jiří Macháček <machaj52@fit.cvut.cz>
2023-10-19 20:19:38 +0200

    Rewrite Program.cs

--- Goldenwood/Program.cs
+++ Goldenwood/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Goldenwood;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+
+namespace Goldenwood // Note: actual namespace depends on the project name.
+{
+    //TODO: Použít using na vytvoření dbContextu
+    //TODO: Vytvořit nějakou globální konstantu na playerID
+    //TODO: Přidat funkcionalitu vložení nějakých dat do DB od spuštění aplikace
+    public class Program
+    {
+        public static int playerId = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+        }
+    }
+}
+
+
+
