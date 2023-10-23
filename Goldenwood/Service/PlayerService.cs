bf6bdf92c82de0d52f55f0d73ba6d5f7b818b5d8
Jiří Macháček <machaj52@fit.cvut.cz>
2023-10-23 11:05:19 +0200

    Implement basic services

---
+++ Goldenwood/Service/PlayerService.cs
@@ -0,0 +1,20 @@
+﻿using Goldenwood.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goldenwood.Service
+{
+    public class PlayerService
+    {
+        private ApplicationDbContext dbContext;
+
+        public PlayerService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
